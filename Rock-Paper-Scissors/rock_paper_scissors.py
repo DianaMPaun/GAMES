8c87a6fc1d4709a56c3f92cccee10c1002319f37
DianaMPaun <157704774+DianaMPaun@users.noreply.github.com>
2024-11-11 15:09:03 +0100

    Update rock_paper_scissors.py

--- Rock-Paper-Scissors/rock_paper_scissors.py
+++ Rock-Paper-Scissors/rock_paper_scissors.py
@@ -33,7 +33,7 @@ if st.button("Play"):
 
     if computer == you:
         st.title("It's a tie!")
-        st.image("https://github.com/DianaMPaun/GAMES/blob/main/Rock-Paper-Scissors/confused.png")
+        st.image("Rock-Paper-Scissors/confused.png")
     elif (computer == "scissors" and you == "paper") or \
          (computer == "paper" and you == "rock") or \
          (computer == "rock" and you == "scissors"):
